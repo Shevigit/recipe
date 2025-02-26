a562b475f0ac64bfb6ad77f899eba87de2373421
בת שבע גרינוולד <BatshevaGr@smg.local>
2025-02-26 19:47:44 +0200

    2/getAllRecipes

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -1,10 +1,10 @@
 const express=require('express');
 const router=express.Router();
 
-const {addRecipe, deleteRecipe, updateRecipe}=require('../controllers/recipeController');
+const {addRecipe, deleteRecipe, updateRecipe,getAllRecipe}=require('../controllers/recipeController');
 
 router.post('/',addRecipe);
 router.delete('/',deleteRecipe);
 router.put('/',updateRecipe);
-
+router.get('/',getAllRecipe);
 module.exports=router;
