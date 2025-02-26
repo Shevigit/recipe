1b5b3f28dfe882b6727da97a8159ba8ff1e01f7e
בת שבע גרינוולד <BatshevaGr@smg.local>
2025-02-26 19:22:40 +0200

    addRecipe

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,11 +1,7 @@
-exports.addRecipe=(req,res)=>{
+const Recipe=require("../models/Recipe")
 
+exports.addRecipe = async(req,res)=>{
+    const recipe  = await Recipe.create(req.body);
+    res.json(recipe)
 }
 
-exports.deleteRecipe=(req,res)=>{
-
-}
-
-exports.updateRecipe=(req,res)=>{
-
-}
